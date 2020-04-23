f65d5fb69541badfc37ace72ae4b27defec7f7b0
leye195 <leye195@naver.com>
2020-04-24 01:03:33 +0900

    카카오 2019 인턴 문제 방배정.py

--- 알고리즘 문제풀이/kakao/방배정.py
+++ 알고리즘 문제풀이/kakao/방배정.py
@@ -1,34 +1,13 @@
-def solution1(k, room_number):
-    answer = [-1 for i in range(0,len(room_number))]
-    room ,idx= [0 for i in range(0,k)],0
-    for r in room_number:
-        if room[r-1]==0:
-            answer[idx]=r
-            room[r-1]=1
-        else:
-            if(0 in room[r:]):
-                i = room[r:].index(0)
-                answer[idx]=i+1+r
-                room[i+r]=1
-            else:
-                break
-        idx+=1
+def solution(k, room_number):
+    answer,rooms=[],{}
+    for i in room_number:
+        n = i
+        visit = [n]
+        while n not in rooms:
+            n = rooms[n]
+            visit.append(n)
+        answer.append(n)
+        for j in visit:
+            rooms[j]=n+1
     return answer
 
-def solution2(k, room_number):
-    answer = [-1 for i in range(0,len(room_number))]
-    room ,idx= {0:[i+1 for i in range(0,k)],1:[]},0
-    for r in room_number:
-        if r in room[0]:
-            answer[idx]=r
-            room[1].append(r)
-            room[0].pop(room[0].index(r))
-        else:
-            for i in range(0,len(room[0])):
-                if room[0][i]>r:
-                    room[1].append(room[0][i])
-                    answer[idx]=room[0][i]
-                    room[0].pop(i)
-                    break
-        idx+=1
-    return answer
