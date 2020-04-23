4b1ea39f684e867f044ad3fb2cb2ff32ebf5eced
leye195 <leye195@naver.com>
2020-04-23 19:43:14 +0900

    불량 아이디.py 주석 설명 추가

--- 알고리즘 문제풀이/kakao/불량아이디.py
+++ 알고리즘 문제풀이/kakao/불량아이디.py
@@ -1,5 +1,7 @@
 # 백트랙 이용 n개 중에서 m개를 뽑아 비교 불량 아이디와 비교 진행
-
+# check_user 함수를 이용해 먼저 user와 banned_user의 총 길이를 비교하고 길이가 같은 경우
+# - 글자를 하나 하나 비교 진행 if 서로 비교하는 문자가 같지 않고 * 도 아닌 경우 False 반환
+# - 문제가 없는 경우 True를 반환
 def check_user(user,banned):
     if len(user)==len(banned):
         for i in range(0,len(user)):
