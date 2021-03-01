ca7862241fb2f6c394578d6032a5ccc3a80f61e9
leye195 <leye195@naver.com>
2021-03-01 14:52:59 +0900

    Revert "leetcode day1"
    This reverts commit bba8775d58071209b05f6fd1d58806350d2aca72.

--- 알고리즘 문제풀이/LeetCode/findDuplicate.py
+++
@@ -1,8 +0,0 @@
-#link: https://leetcode.com/problems/find-the-duplicate-number/submissions/
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range(0,len(nums)-1):
-            if nums[i]==nums[i+1]:
-                return nums[i]
-        
--- 알고리즘 문제풀이/LeetCode/missingNumber.py
+++
@@ -1,7 +0,0 @@
-#link: https://leetcode.com/problems/missing-number/submissions/
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        flag = [0 for i in range(0,len(nums)+1)]
-        for i in nums:
-            flag[i] = 1
-        return flag.index(0)
